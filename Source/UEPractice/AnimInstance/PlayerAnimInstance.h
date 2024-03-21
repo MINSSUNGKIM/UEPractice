038e4a27c97e3cecd43578f55bc84bd01677cedb
minssungkim <arstud13123123123ent@gmail.com>
2024-03-21 15:25:23 +0900

    애니메이션 제작중

---
+++ Source/UEPractice/AnimInstance/PlayerAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+//한글테스트
+#pragma once
+
+#include "../GameInfo.h"
+#include "Animation/AnimInstance.h"
+#include "PlayerAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEPRACTICE_API UPlayerAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
